e9851aa677f3a0d7145f36d58a737db6bf43c1bf
Nathan Warren <nathan.warren222@gmail.com>
2021-02-09 02:52:58 +0000

    polling

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from flask import Flask
 from flask import jsonify
+from flask import request, render_template
 import pandas as pd
 import wikipedia
+import time
+import datetime
 
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps'
 
 @app.route('/name/<value>')
 def name(value):
@@ -33,6 +31,61 @@ def pandas_sugar():
 def bob():
     val = {"value":"bob"}
     return jsonify(val)
+
+@app.route('/')
+def root():
+    return render_template("poll.html", data = poll_data)
+poll_data = {
+   'question' : 'What is your favorite color?',
+   'fields'   : ['Red', 'Blue', 'Orange', 'Green', 'Purple']
+}
+filename = 'data.txt'
+
+@app.route('/poll')
+def poll():
+    vote = request.args.get('field')
+    save = open(filename, 'a')
+    save.write(vote + '\n' )
+    save.close()
+    return render_template('thankyou.html', data=poll_data)
+
+@app.route('/results')
+def show_results():
+    votes = {}
+    for f in poll_data['fields']:
+        votes[f] = 0
+
+    f  = open(filename, 'r')
+    for line in f:
+        vote = line.rstrip("\n")
+        votes[vote] += 1
+
+    return render_template('results.html', data=poll_data, votes=votes)
+
+# @app.route('/time_between')
+# def time_between():
+#     # dates to be comapred
+#     date_now = datetime.datetime.now()
+#     d = request.args.get('d', date_now)
+#     d = datetime.datetime.strptime(d, "%d-%m-%Y %H:%M:%S")
+
+#     # Get difference in units that can be understood easily
+#     difference = d - date_now
+#     duration_in_s = difference.total_seconds()
+
+#     days    = divmod(duration_in_s, 86400)        
+#     hours   = divmod(days[1], 3600)               
+#     minutes = divmod(hours[1], 60)                
+#     seconds = divmod(minutes[1], 1)  
+#     return (
+#         """<form action="" method="get">
+#             Figure out how much time between two dates!
+#             Enter your time and date: MM-DD-YYYY H:M:S <br><br>
+#             Input: <input type="text" name="d">
+#             <input type="submit" value="Calculate">
+#         </form>"""
+#     + f"Days: {days[0]}, Hours: {hours[0]}, Minutes: {minutes[0]}, Seconds: {seconds[0]}"
+#     )
 
 @app.route('/wikipedia/<company>')
 def wikipedia_route(company):
