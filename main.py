e0766bcbe1b0948fb41a77ec0dd00154a29194b9
Nathan Warren <nathan.warren222@gmail.com>
2021-02-09 04:21:37 +0000

    remove access file and excess code

--- main.py
+++ main.py
@@ -9,29 +9,6 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route('/name/<value>')
-def name(value):
-    val = {"value": value}
-    return jsonify(val)
-
-@app.route('/html')
-def html():
-    """Returns some custom HTML"""
-    return """
-    <title>This is a Hello World World Page</title>
-    <p>Hello</p>
-    <p><b>World</b></p>
-    """
-@app.route('/pandas')
-def pandas_sugar():
-    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
-    return jsonify(df.to_dict())
-
-@app.route('/bob')
-def bob():
-    val = {"value":"bob"}
-    return jsonify(val)
-
 @app.route('/')
 def root():
     return render_template("poll.html", data = poll_data)
@@ -61,46 +38,6 @@ def show_results():
         votes[vote] += 1
 
     return render_template('results.html', data=poll_data, votes=votes)
-
-# @app.route('/time_between')
-# def time_between():
-#     # dates to be comapred
-#     date_now = datetime.datetime.now()
-#     d = request.args.get('d', date_now)
-#     d = datetime.datetime.strptime(d, "%d-%m-%Y %H:%M:%S")
-
-#     # Get difference in units that can be understood easily
-#     difference = d - date_now
-#     duration_in_s = difference.total_seconds()
-
-#     days    = divmod(duration_in_s, 86400)        
-#     hours   = divmod(days[1], 3600)               
-#     minutes = divmod(hours[1], 60)                
-#     seconds = divmod(minutes[1], 1)  
-#     return (
-#         """<form action="" method="get">
-#             Figure out how much time between two dates!
-#             Enter your time and date: MM-DD-YYYY H:M:S <br><br>
-#             Input: <input type="text" name="d">
-#             <input type="submit" value="Calculate">
-#         </form>"""
-#     + f"Days: {days[0]}, Hours: {hours[0]}, Minutes: {minutes[0]}, Seconds: {seconds[0]}"
-#     )
-
-@app.route('/wikipedia/<company>')
-def wikipedia_route(company):
-
-    # Imports the Google Cloud client library
-    from google.cloud import language
-    result = wikipedia.summary(company, sentences=10)
-
-    client = language.LanguageServiceClient()
-    document = language.Document(
-        content=result,
-        type_=language.Document.Type.PLAIN_TEXT)
-    encoding_type = language.EncodingType.UTF8
-    entities = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type}).entities
-    return str(entities)
 
 
 if __name__ == '__main__':
